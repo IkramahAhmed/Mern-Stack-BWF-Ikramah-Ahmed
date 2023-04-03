9eda86b113d8fa132adf4f874a16de48fbd5e4da
Ikramah <92651495+IkramahAhmed@users.noreply.github.com>
2023-04-03 18:46:04 +0500

    Add files via upload

---
+++ task-08 JavaScript DOM Manipulation/javasript-project-01-Quote generator/script.js
@@ -0,0 +1,38 @@
+
+
+
+
+// <!-- Project-1 Quote generator -->
+
+
+
+//     const btn=document.querySelector("#new-quote");
+// const quote=document.querySelector(".quote");
+// const person=document.querySelector(".person");
+
+// const quotes =[
+//     {
+//         quote:"eaque expedita libero quaera ",
+//         person:"ali"
+     
+//     },
+//     {
+//         quote:"dolorem voluptates voluptatum cumque! ",
+//         person:"amar"
+
+//     },
+//     {
+//         quote:"consectetur adipisicing elit. ",
+//         person:"asalam"
+
+//     }
+// ]
+//     function search(){
+
+// let a=Math.floor(Math.random() *quotes.length);
+// quote.innerText=quotes[a].quote;
+// person.innerText=quotes[a].quote;
+
+//     }
+
+
